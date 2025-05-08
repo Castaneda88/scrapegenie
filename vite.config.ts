5a10a9d19015a4971d2f938ddcccf12d6589b96c
Michael Castaneda <178906498+Castaneda88@users.noreply.github.com>
2025-05-08 05:25:35 -0700

    Fix: set Vite root to project root

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src',
   plugins: [react()],
+  root: '.', // explicitly set root to current dir
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
+    outDir: 'dist',
   },
 });
